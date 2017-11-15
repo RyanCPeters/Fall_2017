5f1d3e20567d3e1e200800f45ffef7bdf80ddb71
Ryan Peters <petersryan84@gmail.com>
2017-11-15 01:54:52 -0800

    Add files via upload

---
+++ css_342/sampleProblems/cmake-build-debug/CMakeFiles/sampleProblems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sampleProblems.dir/main.cpp.obj"
+  "sampleProblems.pdb"
+  "sampleProblems.exe"
+  "sampleProblems.exe.manifest"
+  "libsampleProblems.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sampleProblems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
