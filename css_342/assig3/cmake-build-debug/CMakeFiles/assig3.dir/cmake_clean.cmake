ad9f95ba149e8ac565651b9be5c164be0823e606
Ryan Peters <petersryan84@gmail.com>
2017-11-27 14:40:49 -0800

    syncing with github

---
+++ css_342/assig3/cmake-build-debug/CMakeFiles/assig3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assig3.dir/RCPeters_AnalysisGen.cpp.obj"
+  "CMakeFiles/assig3.dir/RCPeters_GCD.cpp.obj"
+  "CMakeFiles/assig3.dir/RCPeters_gcdDriver.cpp.obj"
+  "assig3.pdb"
+  "assig3.exe"
+  "assig3.exe.manifest"
+  "libassig3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assig3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
