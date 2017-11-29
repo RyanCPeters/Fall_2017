84a661699bcf692424122913489d68b75373baa1
Ryan Peters <petersryan84@gmail.com>
2017-11-28 17:43:52 -0800

    updated assig 4 files and .gitignore file

--- css_342/IterMerge_assig4/mergesortImproved.cpp
+++ css_342/IterMerge_assig4/mergesortImproved.cpp
@@ -9,34 +9,36 @@ using namespace std;
 
 /**
  *
+ * @tparam Comparable Any data type that has a natural order that permits the use of equality opperators.
  * @param data
  */
-<<<<<<< HEAD
-template <class Comparable> // ToDo: implement template
-mergesortImproved::mergesortImproved(vector<Comparable> &data) {
-=======
-//template <class Comparable> // ToDo: implement template
-mergesortImproved::mergesortImproved(vector<int> &data) {
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
+template <class Comparable>
+mergesortImproved<Comparable>::mergesortImproved(vector<Comparable> &data) {
     /* setting control variables where:
-     *  n = total number of elements in data
-     *  two_k = the number of subsection in data after k divisions, expressed in math notation as (2)^k, and derived here using 1<<k
-     *  k = number of divisions.
+     *
+     *                n = total number of elements in data
+     *
+     *  two_raisedTo_k  = the number of subsections in data after k divisions,
+     *                    expressed in math notation as (2)^k, and derived here
+     *                    using 1<<k.
+     *
+     *                k = number of divisions.
+     *
+     * ~~~ The number of elements per subsection is derived from n/two_raisedTo_k ~~~
      */
-    int n = data.size(), k = 0, two_k = 1<<k;
-    while(n/two_k > 10 ) two_k = 1<<(++k);
+    int n = data.size(), k = 0, two_raisedTo_k = 1<<k;
 
-    double twok = 1<<k;
+    // while the number of elements per subsection is >= 10, increment k and
+    // update two_raisedTo_k
+    while(n/two_raisedTo_k >= 10 ) two_raisedTo_k = 1<<(++k);
 
-    /* when k == 0, we will see that twok == 1; and when k == 0 we have finished combining sub-sections of the array*/
-<<<<<<< HEAD
+
+    /* k == 0 we have nearly finished combining sub-sections of the array, k == 0 is the final level of recombination*/
     while(k >= 0){
-=======
-    while(1 <= twok){
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
 
-        auto shift = (n/twok);
-        // correctlySorted tracks the number of sub-sections that return 1 from combineArrays method.
+        auto shift = n/((double)two_raisedTo_k);
+        // correctlySorted is used to track the number of sub-sections that return 1
+        // from combineArrays method.
         int correctlySorted = 0;
         for(double i = 0; i  < n;) {
             int low = (long)i;
@@ -46,9 +48,9 @@ mergesortImproved::mergesortImproved(vector<int> &data) {
         }
         // recall that 1<<k is the number of expected sub-sections for this level of k
         // so only proceed if correctlySorted == 1<<k
-        if(correctlySorted == (1<<k)) {
+        if(correctlySorted == two_raisedTo_k) {
             --k;
-            twok = 1<<k;
+            two_raisedTo_k = 1<<k;
         }
     }
 }
@@ -60,26 +62,14 @@ mergesortImproved::mergesortImproved(vector<int> &data) {
  * @param last
  * @return
  */
-<<<<<<< HEAD
-template <class Comparable> // ToDo: implement template
-int mergesortImproved::combineArrays(vector<Comparable> &data,int first, int last) {
+template <class Comparable>
+int mergesortImproved<Comparable>::combineArrays(vector<Comparable> &data,int first, int last) {
     if(first >= last) return 0;
     if((last - first) == 1 )return 1;
 
     int a1 = first, a2 = (first + (last - first)/2), b1 = a2+1, b2 = last;
 
-    vector<Comparable> d2; // ToDo: change to Comparable when implementing templates
-=======
-//template <class Comparable> // ToDo: implement template
-int mergesortImproved::combineArrays(vector<int> &data,int first, int last) {
-    if(first >= last) return 0;
-    if((last - first) == 1 )return 1;
-
-    int a1 = first, a2 = (first + (last - first)/2), b1 = a2+1, b2 = last,d2Pos = 0;
-
-    vector<int> d2; // ToDo: change to Comparable when implementing templates
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
-
+    vector<Comparable> d2;
     if(data[a2] <= data[b1])return 1;
     if(data[first] > data[last]){
         int len = a2 - a1 +1;
@@ -97,20 +87,13 @@ int mergesortImproved::combineArrays(vector<int> &data,int first, int last) {
             for(int j = i+1; j < last; ++j ){
                 if(data[j] < data[smallest])smallest = j;
             }// end of for j
-<<<<<<< HEAD
             // if smallest is not greater than i, then we have nothing to swap
-=======
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
             if(smallest > i) swap(data,smallest,i);
         }// end of for i
         return 1;
     }// end of insertion block
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
     d2.assign(data.begin()+a1,data.end()-(data.size()-a2));
     int frontD2 = 0, backD2 = a2-a1;
     while((frontD2 <= backD2 || b1 <=b2) && a1<=a2){
@@ -131,13 +114,8 @@ int mergesortImproved::combineArrays(vector<int> &data,int first, int last) {
  * @param low
  * @param hi
  */
-<<<<<<< HEAD
-template <class Comparable> // ToDo: implement template
-void mergesortImproved::swap(vector<Comparable> &data, int low, int hi) {
-=======
-//template <class Comparable> // ToDo: implement template
-void mergesortImproved::swap(vector<int> &data, int low, int hi) {
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
+template <class Comparable>
+void mergesortImproved<Comparable>::swap(vector<Comparable> &data, int low, int hi) {
     int tmp = data[low];
     data[low] = data[hi];
     data[hi] = tmp;
--- css_342/IterMerge_assig4/mergesortImproved.h
+++ css_342/IterMerge_assig4/mergesortImproved.h
@@ -9,21 +9,18 @@
 #include <iosfwd>
 using namespace std;
 
-template<class Comparable>
+
 class mergesortImproved {
 public:
-
-    explicit mergesortImproved( vector<Comparable> &data );
+    template<class Comparable>
+    mergesortImproved( vector<Comparable> &data );
 
 private:
-<<<<<<< HEAD
-
+    void sort();
+    template<class Comparable>
     int combineArrays(vector<Comparable> &data, int first, int last);
-
+    template<class Comparable>
     void swap(vector<Comparable> &data, int low, int hi);
-=======
-    int combineArrays(vector<int> &data,int first, int last);
-    void swap(vector<int> &data,int low, int hi);
->>>>>>> 27c5e5597144fd8e102baf89faa18a91f0efe392
+
 };
 #endif //ITERMERGE_ASSIG4_MERGESORTIMPROVED_H
